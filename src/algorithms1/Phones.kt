751687ab7068710806d8fb844be33342176d2e10
Ilya <zhilovgithub@gmail.com>
2024-02-27 09:25:21 +0300

    algorithms1

--- src/algorithms1/Phones.kt
+++ src/algorithms1/Phones.kt
@@ -1,29 +1,31 @@
 package algorithms1
 
-import kotlin.random.Random
-
-class Node(
-    var nodeValue: Int? = null,
-    private var leftNode: Node? = null,
-    private var rightNode: Node? = null
+class PhoneNode(
+    var name: String? = null,
+    var phones: MutableList<String>? = null,
+    private var leftNode: PhoneNode? = null,
+    private var rightNode: PhoneNode? = null
 ) {
 
-    fun addNode(newNodeValue: Int) {
-        if (nodeValue == null) {
-            nodeValue = newNodeValue
+    fun addNode(newName: String, phoneNumber: String) {
+        if (name == null) {
+            name = newName
+            phones = mutableListOf(phoneNumber)
         } else {
-            if (newNodeValue > nodeValue!!) {
+            if (newName > name!!) {
                 if (rightNode == null) {
-                    rightNode = Node(newNodeValue)
+                    rightNode = PhoneNode(newName, mutableListOf(phoneNumber))
                 } else {
-                    rightNode?.addNode(newNodeValue)
+                    rightNode?.addNode(newName, phoneNumber)
+                }
+            } else if (newName < name!!) {
+                if (leftNode == null) {
+                    leftNode = PhoneNode(newName, mutableListOf(phoneNumber))
+                } else {
+                    leftNode?.addNode(newName, phoneNumber)
                 }
             } else {
-                if (leftNode == null) {
-                    leftNode = Node(newNodeValue)
-                } else {
-                    leftNode?.addNode(newNodeValue)
-                }
+                phones?.add(phoneNumber)
             }
         }
     }
@@ -32,17 +34,17 @@ class Node(
         return maxOf(leftNode?.getTreeHeight() ?: 0, rightNode?.getTreeHeight() ?: 0) + 1
     }
 
-    fun findElement(targetValue: Int): Node? {
-        if (targetValue == nodeValue) {
+    fun findElement(targetName: String): PhoneNode? {
+        if (targetName == name) {
             return this
         }
-        if (targetValue > nodeValue!!) {
-            return rightNode?.findElement(targetValue)
+        if (targetName > name!!) {
+            return rightNode?.findElement(targetName)
         }
-        return leftNode?.findElement(targetValue)
+        return leftNode?.findElement(targetName)
     }
 
-    fun walkThroughTree(treeNumbers: MutableList<Node?> = mutableListOf(), walkType: String): List<Node?> {
+    fun walkThroughTree(treeNumbers: MutableList<PhoneNode?> = mutableListOf(), walkType: String): List<PhoneNode?> {
         when (walkType) {
             "прямой" -> {
                 treeNumbers.add(this)
@@ -64,21 +66,24 @@ class Node(
         return treeNumbers
     }
 
-    fun deleteValue(targetValue: Int) {
-        if (nodeValue == targetValue) {  // deleting root
+    fun deleteValue(targetName: String) {
+        if (name == targetName) {  // deleting root
             if (leftNode == null && rightNode == null) {
-                nodeValue = null
+                name = null
+                phones = null
             } else if (leftNode == null) {
-                nodeValue = rightNode?.nodeValue
+                name = rightNode?.name
+                phones = rightNode?.phones
                 leftNode = rightNode?.leftNode
                 rightNode = rightNode?.rightNode
             } else if (rightNode == null) {
-                nodeValue = leftNode?.nodeValue
+                name = leftNode?.name
+                phones = leftNode?.phones
                 leftNode = leftNode?.rightNode
                 rightNode = leftNode?.leftNode
             } else {
                 val theSmallestNode = rightNode!!.walkThroughTree(walkType = "обратный")[0]
-                theSmallestNode?.nodeValue?.let {
+                theSmallestNode?.name?.let {
                     val nodeParentOfTheSmallestNode = findElementThatChildEquals(it)?.first
                     if (nodeParentOfTheSmallestNode?.leftNode == null) {
                         nodeParentOfTheSmallestNode?.rightNode = theSmallestNode.rightNode
@@ -86,13 +91,16 @@ class Node(
                         nodeParentOfTheSmallestNode.leftNode = null
                     } else {
                         nodeParentOfTheSmallestNode.rightNode = theSmallestNode.rightNode
-                        nodeParentOfTheSmallestNode.nodeValue = theSmallestNode.nodeValue
+                        nodeParentOfTheSmallestNode.name = theSmallestNode.name
+                        nodeParentOfTheSmallestNode.phones = theSmallestNode.phones
                     }
                 }
-                nodeValue = theSmallestNode?.nodeValue
+                name = theSmallestNode?.name
+                phones = theSmallestNode?.phones
             }
-        } else {
-            val pair = findElementThatChildEquals(targetValue)
+        }
+        else {
+            val pair = findElementThatChildEquals(targetName)
             if (pair == null) {
                 println("Такого элемента нет")
             } else {
@@ -114,39 +122,37 @@ class Node(
         }
     }
 
-    private fun findElementThatChildEquals(targetValue: Int): Pair<Node?, Boolean>? {
-        if (leftNode?.nodeValue == targetValue) {
+    private fun findElementThatChildEquals(targetName: String): Pair<PhoneNode?, Boolean>? {
+        if (leftNode?.name == targetName) {
             return Pair(this, true)
         }
-        if (rightNode?.nodeValue == targetValue) {
+        if (rightNode?.name == targetName) {
             return Pair(this, false)
         }
-        if (targetValue > nodeValue!!) {
-            return rightNode?.findElementThatChildEquals(targetValue)
+        if (targetName > name!!) {
+            return rightNode?.findElementThatChildEquals(targetName)
         }
-        return leftNode?.findElementThatChildEquals(targetValue)
+        return leftNode?.findElementThatChildEquals(targetName)
     }
 }
 
 
 fun main() {
-    val n = 5
-    val numbers = (0 until n).map { Random.nextInt(0, n + 1) }
-    println(numbers)
+    print("Введите количество записей: ")
+    val n = readLine()!!.toInt()
+    val tree = PhoneNode()
 
-    val tree = Node()
-    for (i in numbers) {
-        tree.addNode(i)
+    for (i in 1..n) {
+        print("$i. Введи имя и номер телефона в формате 'имя номер' (без кавычек): ")
+        val (name, phoneNumber) = readLine()!!.split(" ")
+        tree.addNode(name, phoneNumber)
     }
 
-    println(tree.walkThroughTree(walkType = "симметричный").map { it?.nodeValue })
-    println(tree.walkThroughTree(walkType = "обратный").map { it?.nodeValue })
-    println(tree.walkThroughTree(walkType = "прямой").map { it?.nodeValue })
+    println(tree.walkThroughTree(walkType = "симметричный").map { "${it?.name} - ${it?.phones}" })
+    println(tree.findElement("Илья"))
+    println(tree.findElement("Егор"))
+    tree.deleteValue("Илья")
 
-    println(tree.getTreeHeight())
-    println(tree.findElement(7))
-    tree.deleteValue(7)
-
-    // for check that node deleted
-    println(tree.walkThroughTree(walkType = "симметричный").map { it?.nodeValue })
+    println("После удаления:")
+    println(tree.walkThroughTree(walkType = "симметричный").map { "${it?.name} - ${it?.phones}" })
 }
