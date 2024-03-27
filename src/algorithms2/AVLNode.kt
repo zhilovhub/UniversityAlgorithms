7f9fafeee617f5dc56659fa6a6d4356d072c6fd7
Ilya Zhilov <zhilovgithub@gmail.com>
2024-03-27 22:03:29 +0300

    trying swap parent after balance

--- src/algorithms2/AVLNode.kt
+++ src/algorithms2/AVLNode.kt
@@ -45,9 +45,13 @@ class AVLNode(
         this.rightNode = this.leftNode
         this.leftNode = this.rightNode?.leftNode
         this.rightNode?.leftNode = this.rightNode?.rightNode
+        this.rightNode?.leftNode?.parent = this.leftNode
         this.rightNode?.rightNode = temp
+        this.rightNode?.rightNode?.parent = this.rightNode
         this.rightNode?.updateHeight()
         this.rightNode?.updateBalance()
+        this.rightNode?.parent = this
+        this.leftNode?.parent = this
         updateHeight()
         updateBalance()
     }
@@ -58,9 +62,13 @@ class AVLNode(
         this.leftNode = this.rightNode
         this.rightNode = this.leftNode?.rightNode
         this.leftNode?.rightNode = this.leftNode?.leftNode
+        this.leftNode?.rightNode?.parent = this.leftNode
         this.leftNode?.leftNode = temp
+        this.leftNode?.leftNode?.parent = this.leftNode
         this.leftNode?.updateHeight()
         this.leftNode?.updateBalance()
+        this.rightNode?.parent = this
+        this.leftNode?.parent = this
         updateHeight()
         updateBalance()
     }
@@ -125,38 +133,37 @@ class AVLNode(
     }
 
     fun deleteValue(targetValue: Int): AVLNode? {
-        if (nodeValue != targetValue && leftNode == null && rightNode == null) {
-            println("Такого элемента нет")
+        if (nodeValue == null) {
             return null
-        }
-        else if (leftNode )
-        if (nodeValue == targetValue) {
+        } else if (targetValue < nodeValue!!) {
+            leftNode = leftNode?.deleteValue(targetValue)
+        } else if (targetValue > nodeValue!!) {
+            rightNode = rightNode?.deleteValue(targetValue)
+        } else {
             if (leftNode == null && rightNode == null) {
-                nodeValue = null
-            } else if (leftNode == null) {
+                if (parent?.leftNode == this) {
+                    parent?.leftNode = null
+                } else if (parent?.rightNode == this) {
+                    parent?.rightNode = null
+                }
+                return null
+            }
+            else if (leftNode == null) {
                 nodeValue = rightNode?.nodeValue
                 leftNode = rightNode?.leftNode
                 rightNode = rightNode?.rightNode
             } else if (rightNode == null) {
                 nodeValue = leftNode?.nodeValue
-                leftNode = leftNode?.rightNode
-                rightNode = leftNode?.leftNode
+                leftNode = leftNode?.leftNode
+                rightNode = leftNode?.rightNode
             } else {
                 val theSmallestNode = rightNode!!.getSmallest()
-                theSmallestNode?.nodeValue?.let {
-                    val nodeParentOfTheSmallestNode = findElementThatChildEquals(it)?.first
-                    if (nodeParentOfTheSmallestNode?.leftNode == null) {
-                        nodeParentOfTheSmallestNode?.rightNode = theSmallestNode.rightNode
-                    } else if (nodeParentOfTheSmallestNode.rightNode == null) {
-                        nodeParentOfTheSmallestNode.leftNode = null
-                    } else {
-                        nodeParentOfTheSmallestNode.rightNode = theSmallestNode.rightNode
-                        nodeParentOfTheSmallestNode.nodeValue = theSmallestNode.nodeValue
-                    }
-                }
-                nodeValue = theSmallestNode?.nodeValue
+                nodeValue = theSmallestNode.nodeValue
+                theSmallestNode.deleteValue(theSmallestNode.nodeValue!!)
+                theSmallestNode.parent?.leftNode = null
             }
         }
+        return this
     }
 
     private fun getSmallest(): AVLNode {
@@ -188,6 +195,23 @@ fun main() {
     for (i in numbers) {
         tree.addNode(i)
     }
+
+    print(tree.walkThroughTree(walkType = "прямой").map {
+        "(${Pair("Значение узла ${it?.nodeValue}", "Баланс ${it?.balance}")})"
+    })
+    println(" - Прямой обход")
+
+    print(tree.walkThroughTree(walkType = "симметричный").map {
+        "(${Pair("Значение узла ${it?.nodeValue}", "Баланс ${it?.balance}")})"
+    })
+    println(" - Симметричный обход")
+
+    tree.deleteValue(10)
+
+    print(tree.walkThroughTree(walkType = "симметричный").map {
+        "(${Pair("Значение узла ${it?.nodeValue}", "Баланс ${it?.balance}")})"
+    })
+    println(" - Симметричный обход")
 
     print(tree.walkThroughTree(walkType = "прямой").map {
         "(${Pair("Значение узла ${it?.nodeValue}", "Баланс ${it?.balance}")})"
